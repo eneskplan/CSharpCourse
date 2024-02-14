9f6275f456654d524e6bde28cbba01b306b05fc4
Enes Kaplan <enesskaplann02@gmail.com>
2024-02-14 17:41:28 +0300

    Add project files.

---
+++ Classes/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Customer
+    {
+        
+        public int Id { get; set; } //bu ise bir property yani özellik tanımlamaktır.
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+        
+        //{
+        //    get { return "Mr." + _firstName; }
+        //    set { _firstName = value; }
+        //}
+    }
+}
